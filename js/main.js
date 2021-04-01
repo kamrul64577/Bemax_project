d428e8046d4ac051642a2be911c1c6683ab7293d
Kamrul <53050059+kamrul64577@users.noreply.github.com>
2021-04-01 17:17:10 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,87 @@
+
+const TypeWriter = function(txtElement, words, wait=3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+}
+
+//type method
+TypeWriter.prototype.type = function() {
+    //current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of currrent word
+    const fullTxt = this.words[current];
+
+    //check if deleting
+    if(this.isDeleting) {
+        //remove char
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } 
+     else {
+         //add char
+         this.txt = fullTxt.substring(0, this.txt.length + 1);
+     }
+
+     //Insert txt into element
+     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+     //Typespeed
+     let typeSpeed = 200;
+
+     if(this.isDeleting) {
+         typeSpeed /=2;
+     }
+
+     //if word is complete
+     if(!this.isDeleting && this.txt === fullTxt) {
+         //make pause at end
+         typeSpeed = this.wait;
+         //set delete to true
+         this.isDeleting = true;
+
+     }
+     else if(this.isDeleting && this.txt === '') {
+         this.isDeleting = false;
+         //move to next word
+         this.wordIndex++;
+         //pause before start typing
+         typeSpeed = 500;
+
+     }
+
+
+     setTimeout(() => this.type(), typeSpeed)
+
+
+}
+
+
+//Init on DOM load
+document.addEventListener('DOMContentLoaded', init);
+
+//init App
+function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    //Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+}
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    var mixer = mixitup('.portfolio-item');
+});
